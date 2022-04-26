d0d83eeabed01eb114d1df565f2de25077e27d09
Josef Al-sader <alsader.josef@gmail.com>
2022-04-26 23:45:19 +0200

    Code template for tutorial

---
+++ src/main/java/com/nexer/tutorial/aws/functions/SimpleLambdaFunction.java
@@ -0,0 +1,40 @@
+package com.nexer.tutorial.aws.functions;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+//import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
+
+/**
+ * This class showcases a simple lambda function
+ */
+public class SimpleLambdaFunction implements RequestHandler<Void, Void> {
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleLambdaFunction.class);
+  private static final SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+  private LambdaClient lambdaClient;
+
+  @Override
+  public Void handleRequest(Void input, Context context) {
+    if (lambdaClient == null) {
+      lambdaClient = LambdaClient.builder().httpClient(httpClient).build();
+    }
+    try {
+      LOG.info("Hello World");
+
+      // TODO (Workshop 1):
+      //  Get account settings
+      //  Publish total lambda storage to CloudWatch
+      //  Publish current usage of lambda storage to CloudWatch
+      //  Publish Remaining usage of lambda storage to CloudWatch
+      //  Hint: Use the commented out library GetAccountSettingsResponse together with the lambda client.
+      //  you will also need an embeddedMetricsLogger
+    } catch(Exception e) {
+      LOG.error("Exception: could not greet world" + e);
+    }
+    return null;
+  }
+}
---
+++ src/test/java/com/nexer/tutorial/aws/functions/SimpleLambdaFunctionTest.java
@@ -0,0 +1,17 @@
+package com.nexer.tutorial.aws.functions;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SimpleLambdaFunctionTest {
+
+  @BeforeClass
+  public static void setupClass() {
+  }
+
+  @Test
+  public void ShouldLogAccountSettingsTest() {
+
+  }
+
+}
