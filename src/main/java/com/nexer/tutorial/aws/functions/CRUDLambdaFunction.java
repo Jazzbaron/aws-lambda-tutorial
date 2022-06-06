1e45f765c2770d76ceb0651a74a464705dcaac86
Josef Al-sader <alsader.josef@gmail.com>
2022-06-06 23:38:47 +0200

    Workshop 2 prep.

---
+++ src/main/java/com/nexer/tutorial/aws/functions/CRUDLambdaFunction.java
@@ -0,0 +1,71 @@
+package com.nexer.tutorial.aws.functions;
+
+import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+
+public class CRUDLambdaFunction implements RequestHandler<Void, String> {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleLambdaFunction.class);
+    private static final SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+    private AmazonDynamoDB ddb;
+
+    @Override
+    public String handleRequest(Void input, Context context) {
+        if (ddb == null) {
+            ddb = AmazonDynamoDBClientBuilder.defaultClient();
+        }
+
+        try {
+
+            // TODO (Workshop Day 2):
+            // Task 1: Get the total table count of your account (should be 1).
+            // Use the currentTablesInUse method to do this.
+            // Make sure the table is created! Check the SAM cloudformation template and Management Console.
+            // You will need to setup the lambda function in template.yml and API in api.yml
+            // Use SimpleLambdaFunction as inspiration
+
+            // Task 2: Querying "myDynamoDBTable" (the one we created)
+            // Read a record
+            // Create a record
+            // Delete a record
+
+            // Task 3: Create CRUD-operation APIs' for each operation
+
+        } catch(Exception e) {
+            LOG.error("Exception: Could not check for account settings: " + e);
+        }
+        return null;
+    }
+
+    // TODO (Workshop 2) fill in the missing part of the method stub:
+    // Hint: More reading on the ListTable API -- https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_ListTables.html
+    public int currentTablesInUse() {
+        int tableCount = 0;
+        String lastEvaluatedTableName = null;
+
+        do {
+            // Create a new request:
+            ListTablesRequest request = new ListTablesRequest()
+                    .withExclusiveStartTableName(lastEvaluatedTableName);
+
+            // Get all the tables:
+            // ...
+
+            // Count them:
+            // ...
+
+        } while (lastEvaluatedTableName != null);
+
+        return tableCount;
+    }
+}
--- src/main/java/com/nexer/tutorial/aws/functions/SimpleLambdaFunction.java
+++ src/main/java/com/nexer/tutorial/aws/functions/SimpleLambdaFunction.java
@@ -7,7 +7,11 @@ import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.services.lambda.LambdaClient;
-//import software.amazon.awssdk.services.lambda.model.GetAccountSettingsResponse;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 
 /**
  * This class showcases a simple lambda function
@@ -24,23 +28,24 @@ public class SimpleLambdaFunction implements RequestHandler<Void, String> {
     }
     try {
 
+      // TODO [Optional] (Before Workshop):
+      // Modify Code to return input parameter from user
       String message = "Hello Nexer!";
 
       return message;
 
-      // TODO [Optional] (Before Workshop):
-      //  Modify Code to return input parameter from user
-
-      // TODO (Workshop):
+      // TODO (Workshop Day 1):
       //  Get account settings
       //  > Publish total lambda storage to CloudWatch
       //  > Publish current usage of lambda storage to CloudWatch
       //  > Publish Remaining usage of lambda storage to CloudWatch
       //  Hint: Use the commented out library GetAccountSettingsResponse together with the lambda client.
-      //  you will also need an embeddedMetricsLogger
+      //  you will also need an embeddedMetricsLoggerç
+
     } catch(Exception e) {
       LOG.error("Exception: could not greet world" + e);
     }
     return null;
   }
+
 }
